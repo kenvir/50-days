8148c835eba9a1b597df84bb17ff54f2d9c80013
kenvir <viet02092001@gmail.com>
2021-12-22 20:40:47 +0700

    update

--- day 21/script.js
+++ day 21/script.js
@@ -12,10 +12,9 @@ for (empty of empties) {
 }
 
 function dragStart() {
-    this.className += ' hold' 
-    setTimeout(() => this.className = 'invisible', 0)
+    this.className += ' hold'
+    setTimeout(() => this.className = 'invisible', 0) 
 }
-
 function dragEnd() {
     this.className = 'fill'
 }
